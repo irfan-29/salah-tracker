e9f6382f2c66d1b847fc52829867bde59bfb46ff
unknown <mdirfan29pro@gmail.com>
2025-02-06 08:40:25 +0530

    changes-for-render

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.get("/salah", function(req, res){
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 });
