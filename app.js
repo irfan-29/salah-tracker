892d6b524658dab10f302793adb3e0b56fd29986
unknown <mdirfan29pro@gmail.com>
2025-02-07 08:41:30 +0530

    salah-timings-1

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 
 const app=express();
 
@@ -21,10 +23,15 @@ const salahSchema = new mongoose.Schema({
 });
 const Salah = mongoose.model("Salah", salahSchema);
 
+const locationSchema = new mongoose.Schema({
+    location: String
+});
+const Location = mongoose.model("Location", locationSchema);
+
 
 
 app.get("/", function(req, res){
-    res.render("salah", {keySalah: ""});
+    res.render("salah", {keySalah: "", keyLocation: 1258740});
 });
 
 app.post("/salah", function(req, res){
@@ -49,8 +56,46 @@ app.get("/salah", function(req, res){
     //     res.status(500).send('Error occurred while fetching Salah');
     // }
     Salah.find({}).then((salah) => {
-        res.render('salah', { keySalah: salah });
+        res.render('salah', { keySalah: salah, keyLocation: "1258740" });
     });
+});
+
+app.post("/location", function(req, res) {
+    const location = req.body.location;
+    
+    Location.findOneAndUpdate({}, { location: location }, { sort: { _id: -1 }, upsert: true })
+        .then(() => {
+            res.redirect("/salah-timings");
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error updating location.");
+        });
+});
+
+
+app.get("/salah-timings", async(req, res)=>{
+    try{
+    Location.findOne({}).then(async (location) => {
+        const response = await fetch(`https://www.islamicfinder.org/prayer-widget/${location.location}/shafi/3/0/18.0/18.0`);
+        const data = await response.text();
+
+        const $ = cheerio.load(data);
+            const prayerTimes = [];
+            
+            $(".d-flex.flex-direction-row").each((index, element) => {
+                const timeType = $(element).find("p").first().text().trim(); // Fajr, Dhuhr, etc.
+                const time = $(element).find("p").last().text().trim(); // Prayer time (e.g., 05:29 AM)
+                prayerTimes.push({ timeType, time });
+            });
+            $("span:contains('Powered By')").remove();
+            // console.log(data);
+        res.render('salah-timings', { keyLocation: location!=null ? location.location : "1258740", keyPrayerTimes: prayerTimes, keyData: data });
+    });
+    }catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching prayer times.');
+    }
 });
 
 
