29403b203f6e763e7f01c0f7875405b5515965e6
unknown <mdirfan29pro@gmail.com>
2025-04-13 22:53:19 +0530

    vercel-deploy-2

--- app.js
+++ app.js
@@ -229,6 +229,9 @@ app.get("/navbar", function(req, res){
     res.render('navbar');
 });
 
+
+module.exports = app;
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 });
